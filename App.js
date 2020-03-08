c8c955589d7eafc022dc67684ef3591e3aadff92
filipebotti <filipebotti@hotmail.com>
2020-03-08 15:30:10 -0300

    Initial Commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SafeAreaView, StatusBar } from 'react-native'
+
+const App = () => (
+  <>
+    <StatusBar barStyle="dark-content" />
+    <SafeAreaView />
+  </>
+);
+
+
+export default App;
